2a3823d8b65994bafb4cd955bd76c534f3ac7ea6
tuxafgmur <tuxafgmur@users.noreply.github.com>
2016-04-02 19:04:35 +0200

    Update custom-recovery.sh

--- custom_recovery.sh
+++ custom_recovery.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copyright (C) 2016 Tuxafgmur - Dhollmen 
 # Modify recovery before build image
+# Parameter #1 is the TARGET_RECOVERY_OUT directory
+# Parameter #2 can be 'trwp' if defined RECOVERY_VARIANT 
 
 rm -f $1/root/sbin/cbd
-cp vendor/cm/prebuilt/common/etc/mkshrc $1/root/etc/
