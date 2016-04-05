945364c0ab97963417eb597b7a15680c13cc6731
tuxafgmur <tuxafgmur@users.noreply.github.com>
2016-04-06 01:01:37 +0200

    Allows recovery to better device recognition

--- custom_recovery.sh
+++ custom_recovery.sh
@@ -5,3 +5,11 @@
 # Parameter #2 can be 'trwp' if defined RECOVERY_VARIANT 
 
 rm -f $1/root/sbin/cbd
+
+if [ `echo $1 | grep espresso3g` ]; then
+    sed "s/ro.build.product=.*/ro.build.product=espressorf/"       -i $1/root/default.prop
+    sed "s/ro.product.device=.*/ro.product.device=espresso10rf/"   -i $1/root/default.prop
+else
+    sed "s/ro.build.product=.*/ro.build.product=espressowifi/"     -i $1/root/default.prop
+    sed "s/ro.product.device=.*/ro.product.device=espresso10wifi/" -i $1/root/default.prop
+fi
