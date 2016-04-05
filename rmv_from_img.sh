48506c1336c40c4e19441f55c4b6fb6c3a6ba479
tuxafgmur <tuxafgmur@users.noreply.github.com>
2016-04-06 01:07:52 +0200

    Build.prop - updates some values

--- rmv_from_img.sh
+++ rmv_from_img.sh
@@ -3,7 +3,6 @@
 # Removes unneeded files from the rom, before beeing packaged
 
 WSYSTEMDIR=$1
-WOURTDIR=${WSYSTEMDIR%/system}
 
 rm -f  $WSYSTEMDIR/recovery-from-boot.p
 rm -f  $WSYSTEMDIR/bin/install-recovery.sh
@@ -24,15 +23,18 @@ rm -f  $WSYSTEMDIR/xbin/{ls,ps}
 [ -e $WSYSTEMDIR/xbin/fstrim ] || ln -s ./busybox $WSYSTEMDIR/xbin/fstrim
 [ -e $WSYSTEMDIR/vendor/lib/libPVRScopeServices.so ] || ln -s ./libPVRScopeServices_SGX540_120.so $WSYSTEMDIR/vendor/lib/libPVRScopeServices.so
 
-sed '/dalvik.vm.stack-trace-file/d' -i $WSYSTEMDIR/build.prop
-sed '/ro.expect.recovery_id/d'      -i $WSYSTEMDIR/build.prop
-sed 's;dalvik.vm.heapstartsize=5m;dalvik.vm.heapstartsize=8m;'       -i $WSYSTEMDIR/build.prop
-sed 's;dalvik.vm.heapsize=256m;dalvik.vm.heapsize=174m;'             -i $WSYSTEMDIR/build.prop
-sed 's;dalvik.vm.heapgrowthlimit=48m;dalvik.vm.heapgrowthlimit=64m;' -i $WSYSTEMDIR/build.prop
-sed 's;wifi.supplicant_scan_interval=15;wifi.supplicant_scan_interval=180;' -i $WSYSTEMDIR/build.prop
-
 FirstSortLine=`grep -n 'Additional Build Properties' $WSYSTEMDIR/build.prop | cut -d: -f1`
 LastSortLine=`wc -l $WSYSTEMDIR/build.prop | cut -d ' ' -f1`
 head -n $FirstSortLine $WSYSTEMDIR/build.prop > $WSYSTEMDIR/build.new
 head -n $((LastSortLine)) $WSYSTEMDIR/build.prop | tail -n +$((FirstSortLine + 1)) | tr '\n[' '[\n' | sort | tr '\n[' '[\n' | grep -v '^\[$' | sort >> $WSYSTEMDIR/build.new
 rm $WSYSTEMDIR/build.prop && mv $WSYSTEMDIR/build.new $WSYSTEMDIR/build.prop
+
+sed '/dalvik.vm.stack-trace-file/d'                                         -i $WSYSTEMDIR/build.prop
+sed '/ro.expect.recovery_id/d'                                              -i $WSYSTEMDIR/build.prop
+sed 's/dalvik.vm.heapstartsize=.*/dalvik.vm.heapstartsize=8m/'              -i $WSYSTEMDIR/build.prop
+sed 's/dalvik.vm.heapgrowthlimit=.*/dalvik.vm.heapgrowthlimit=64m/'         -i $WSYSTEMDIR/build.prop
+sed "s/wifi.supplicant_scan_interval=.*/wifi.supplicant_scan_interval=180/" -i $WSYSTEMDIR/build.prop
+
+if [ `echo $1 | grep espresso3g` ]; then
+    sed "s/ro.build.product=.*/ro.build.product=espressorf/"                -i $WSYSTEMDIR/build.prop
+fi
